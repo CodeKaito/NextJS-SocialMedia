cb709aab928d5c670f76c06af204d86597854643
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-05-17 16:12:11 +0200

    update

--- components/Card.js
+++ components/Card.js
@@ -1,6 +1,6 @@
 export default function Card({ children }) {
     return (
-        <div className='bg-white shadow-lg rounded-md p-4 mb-5'>
+        <div className='bg-white shadow-md shadow-gray-250 hover:shadow-gray-400 rounded-md p-4 mb-5'>
             {children}
         </div>
     )
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,14 @@ export default function Home() {
   return (
     <div className='flex mt-4 max-w-4xl mx-auto gap-6'>
       <div className='w-1/3'>
-        <Card>test links</Card>
+        <Card>
+          <h2 className='text-gray-400 mb-3'>Navigation</h2>
+          <a href="" className="flex gap-2 py-3">Home</a>
+          <a href="" className="flex gap-2 py-3">Friends</a>
+          <a href="" className="flex gap-2 py-3">Saved posts</a>
+          <a href="" className="flex gap-2 py-3">Notifications</a>
+          <a href="" className="flex gap-2 py-3">Logout</a>
+        </Card>
       </div>
       <div className='grow'>
         <Card>form here</Card>
