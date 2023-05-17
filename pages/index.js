e33d899d47bc99497e8378e9ec7e427eb20076a2
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-05-17 15:22:20 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
 export default function Home() {
   return (
-    <div className='flex max-w-4xl mx-auto border-4 border-emerald-500'>
-      <div className='w-1/3'>sidebar</div>
-      <div className=''>form and posts</div>
+    <div className='flex mt-4 max-w-4xl mx-auto border-4 border-emerald-500'>
+      <div className='w-1/3'>
+        <div className='border border-black'>
+          links
+        </div>
+      </div>
+      <div className=''>
+        <div className='border border-black'>
+          form
+        </div>
+      </div>
     </div>
   )
 }
