174abae2a7a598e41282eb66f806b6b9cf09772d
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-05-17 15:32:28 +0200

    update

---
+++ components/Card.js
@@ -0,0 +1,7 @@
+export default function Card({ children }) {
+    return (
+      <div className='bg-white shadow-sm'>
+        {children}
+      </div>
+    )
+  }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
+import Card from "@/components/Card";
+
 export default function Home() {
   return (
     <div className='flex mt-4 max-w-4xl mx-auto border-4 border-emerald-500'>
       <div className='w-1/3'>
-        <div className='border border-black'>
-          links
-        </div>
+        <Card>test links</Card>
       </div>
       <div className=''>
         <div className='border border-black'>
