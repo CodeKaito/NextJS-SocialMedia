92a046f7c8d138684cda18bc7ff6377082c0d7ee
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-05-18 10:57:46 +0200

    Update PostCard.js

--- components/PostCard.js
+++ components/PostCard.js
@@ -4,18 +4,23 @@ import Card from "./Card";
 export default function PostCard() {
     return(
         <Card>
-          <div className='flex gap-3'>
+            <div className='flex gap-3'>
+                <div>
+                <Avatar />
+                </div>
             <div>
-              <Avatar />
+                <p><a className='font-semibold'>John Doe</a> shared a <a className='text-socialBlue'>post</a></p>
+                <p className='text-gray-500 text-sm'>2 hours ago</p>
+            </div>
+
+            
             </div>
             <div>
-              <p><a className='font-semibold'>John Doe</a> shared a <a className='text-socialBlue'>post</a></p>
-              <p className='text-gray-500 text-sm'>2 hours ago</p>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Quidem eligendi quas voluptatem exercitationem quis explicabo animi ex accusamus, a, odit dignissimos, 
-              quibusdam quo eaque debitis tenetur corrupti quamtempore quod.</p>
+                <p className='my-2 text-sm'>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Quidem eligendi quas voluptatem exercitationem quis explicabo animi ex accusamus, a, odit dignissimos, 
+                quibusdam quo eaque debitis tenetur corrupti quamtempore quod.
+                </p>
             </div>
-          </div>
         </Card>
     )
 }
