990dfaaebc6563f543579b36ad43dc7f7a994d78
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-05-18 17:25:16 +0200

    update

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,7 +1,13 @@
-export default function Avatar() {
+export default function Avatar({size}) {
+
+    let width = 'w-12';
+    if (size === 'lg') {
+        width = 'w-24' ;
+    }
+
     return (
-        <div className='w-12 rounded-full overflow-hidden'>
-            <img src="./images/profile.jpg" alt="image" />
+        <div className={`${width} rounded-full overflow-hidden`}>
+            <img src="/images/profile.jpg" alt="image" />
         </div>
     )
 }
--- pages/profile.js
+++ pages/profile.js
@@ -1,3 +1,4 @@
+import Avatar from "@/components/Avatar";
 import Card from "@/components/Card";
 import Layout from "@/components/Layout";
 import Image from "next/image";
@@ -6,8 +7,23 @@ export default function ProfilePage() {
     return (
         <Layout>
             <Card noPadding={true}>
-            <Image src='/images/santorini.jpg' height={800} width={800} alt='profile' className='rounded-md' />
-            John Doe</Card>
+                <div className='relative'>
+                    <div className='h-40 overflow-hidden flex justify-center items-center'>
+                        <Image src='/images/santorini.jpg' height={800} width={800} alt='profile' className='rounded-md' />
+                    </div>
+                    <div className='absolute top-24 left-7 rounded-full border border-gray-400 shadow-lg hover:scale-105'>
+                        <Avatar size={'lg'} />
+                    </div>
+                
+                    <div className='p-4 pb-24'>
+                        <div className='ml-36'>
+                            <h1 className='uppercase font-semibold text-2xl'>
+                                John Doe
+                            </h1>
+                        </div>
+                    </div>
+                </div>
+            </Card>
         </Layout>
     )
 }
