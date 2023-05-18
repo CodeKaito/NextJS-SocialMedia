00df79ff521c83675434839576e620f7d62926a7
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-05-18 10:44:57 +0200

    update

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import PostFormCard from "@/components/PostFormCard";
 export default function Home() {
   return (
     <div className='flex mt-4 max-w-4xl mx-auto gap-6'>
+      <title>PostMate</title>
       <div className='w-1/3'>
         <NavigationCard />
       </div>
@@ -17,9 +18,11 @@ export default function Home() {
               <Avatar />
             </div>
             <div>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                Quidem eligendi quas voluptatem exercitationem quis explicabo animi ex accusamus, a, odit dignissimos, 
-                quibusdam quo eaque debitis tenetur corrupti quam tempore quod.</p>
+              <p><a className='font-semibold'>John Doe</a> shared a <a className='text-socialBlue'>post</a></p>
+              <p>2 hours ago</p>
+              <p>Lorem ipsum, dolor sit amet consectetur       adipisicing elit.
+              Quidem eligendi quas voluptatem exercitationem quis explicabo animi ex accusamus, a, odit dignissimos, 
+              quibusdam quo eaque debitis tenetur corrupti quamtempore quod.</p>
             </div>
           </div>
         </Card>
