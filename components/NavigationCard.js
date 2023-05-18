16cabf48f06305cc90225a67840f44054f397fb4
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-05-18 11:40:30 +0200

    Update NavigationCard.js

--- components/NavigationCard.js
+++ components/NavigationCard.js
@@ -1,15 +1,18 @@
 import Card from "./Card";
+import { AiOutlineHome } from 'react-icons/ai'
+import { BsPeople, BsBookmarks } from 'react-icons/bs'
+import { IoMdNotificationsOutline, IoMdLogOut } from 'react-icons/Io'
 
 export default function NavigationCard() {
     return (
         <Card>
         <div className='px-4 py-2'>
             <h2 className='text-gray-400 mb-3'>Navigation</h2>
-                <a href="" className="flex gap-3 py-3">Home</a>
-                <a href="" className="flex gap-3 py-3">Friends</a>
-                <a href="" className="flex gap-3 py-3">Saved posts</a>
-                <a href="" className="flex gap-3 py-3">Notifications</a>
-                <a href="" className="flex gap-3 py-3">Logout</a>
+                <a href="" className="flex gap-3 py-3"><AiOutlineHome />Home</a>
+                <a href="" className="flex gap-3 py-3"><BsPeople />Friends</a>
+                <a href="" className="flex gap-3 py-3"><BsBookmarks />Saved</a>
+                <a href="" className="flex gap-3 py-3"><IoMdNotificationsOutline />Notifications</a>
+                <a href="" className="flex gap-3 py-3"><IoMdLogOut />Logout</a>
         </div>
         </Card>
     )
