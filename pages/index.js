af3df4654319db77097c8db05c49929c3610604a
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-05-18 10:46:45 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,14 +13,14 @@ export default function Home() {
       <div className='grow'>
         <PostFormCard />
         <Card>
-          <div className='flex'>
+          <div className='flex gap-3'>
             <div>
               <Avatar />
             </div>
             <div>
               <p><a className='font-semibold'>John Doe</a> shared a <a className='text-socialBlue'>post</a></p>
-              <p>2 hours ago</p>
-              <p>Lorem ipsum, dolor sit amet consectetur       adipisicing elit.
+              <p className='text-gray-500 text-sm'>2 hours ago</p>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
               Quidem eligendi quas voluptatem exercitationem quis explicabo animi ex accusamus, a, odit dignissimos, 
               quibusdam quo eaque debitis tenetur corrupti quamtempore quod.</p>
             </div>
