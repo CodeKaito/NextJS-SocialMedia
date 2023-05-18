a07ee4d3e7aeef352169e0d612cab0472d4ad2f9
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-05-18 13:20:36 +0200

    update

--- components/PostFormCard.js
+++ components/PostFormCard.js
@@ -1,5 +1,7 @@
 import Avatar from "./Avatar";
 import Card from "./Card";
+import { BsUniversalAccess, BsFillSunFill } from 'react-icons/bs'
+import { GiPositionMarker } from 'react-icons/gi'
 
 export default function PostFormCard() {
     return (
@@ -12,16 +14,16 @@ export default function PostFormCard() {
             </div>
             <div className='flex gap-5 items-center mt-2'>
                 <div>
-                    <button className='flex gap-1'>People</button>
+                    <button className='flex gap-1'><BsUniversalAccess />People</button>
                 </div>
                 <div>
-                    <button className='flex gap-1'>Check in</button>
+                    <button className='flex gap-1'><GiPositionMarker />Check in</button>
                 </div>
                 <div>
-                    <button className='flex gap-1'>Mood</button>
+                    <button className='flex gap-1'><BsFillSunFill />Mood</button>
                 </div>
                 <div className='grow text-right '>
-                    <button className="bg-socialBlue text-white px-5 py-1 rounded-md">Share post</button>
+                    <button className="bg-socialBlue text-white px-5 py-1 rounded-md">Share a post</button>
                 </div>
             </div>
         </Card>
--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,10 @@ export default function Home() {
   return (
     <div className='flex mt-4 max-w-4xl mx-auto gap-6'>
       <title>PostMate</title>
-      <div className='w-1/3'>
+      <div className='w-3/12'>
         <NavigationCard />
       </div>
-      <div className='grow'>
+      <div className='w-9/12'>
         <PostFormCard />
         <PostCard />
       </div>
