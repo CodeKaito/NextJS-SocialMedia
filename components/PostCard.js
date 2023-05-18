02c9cacbb97a14cd07244cf99f6cf5102ca89e7b
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-05-18 11:18:44 +0200

    update

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,26 +1,50 @@
+import Image from "next/image";
 import Avatar from "./Avatar";
 import Card from "./Card";
 
 export default function PostCard() {
     return(
+        <div>
         <Card>
             <div className='flex gap-3'>
                 <div>
                 <Avatar />
                 </div>
-            <div>
+                <div>
                 <p><a className='font-semibold'>John Doe</a> shared a <a className='text-socialBlue'>post</a></p>
                 <p className='text-gray-500 text-sm'>2 hours ago</p>
-            </div>
-
-            
+                </div>
             </div>
             <div>
-                <p className='my-2 text-sm'>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                <p className='my-4 text-sm'>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                 Quidem eligendi quas voluptatem exercitationem quis explicabo animi ex accusamus, a, odit dignissimos, 
                 quibusdam quo eaque debitis tenetur corrupti quamtempore quod.
                 </p>
+                <div>
+                    <Image src="/images/santorini.jpg" width='900' height='900' alt="santorini" />
+                </div>
             </div>
         </Card>
+        <Card>
+        <div className='flex gap-3'>
+                <div>
+                <Avatar />
+                </div>
+                <div>
+                <p><a className='font-semibold'>John Doe</a> shared a <a className='text-socialBlue'>post</a></p>
+                <p className='text-gray-500 text-sm'>2 hours ago</p>
+                </div>
+            </div>
+            <div>
+                <p className='my-4 text-sm'>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Quidem eligendi quas voluptatem exercitationem quis explicabo animi ex accusamus, a, odit dignissimos, 
+                quibusdam quo eaque debitis tenetur corrupti quamtempore quod.
+                </p>
+                <div>
+                    <Image src="/images/caraibi.jpg" width='900' height='900' alt="santorini" />
+                </div>
+            </div>
+        </Card>
+        </div>
     )
 }
