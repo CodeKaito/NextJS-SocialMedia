387857ed4d4f8174da0aeaa160f3ffd319d0621c
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-05-19 09:26:18 +0200

    update

---
+++ pages/profile/friends.js
@@ -0,0 +1,5 @@
+export default function Friends() {
+    return (
+        <div>I am friends</div>
+    )
+}
---
+++ pages/profile/photo.js
@@ -0,0 +1,5 @@
+export default function Photo() {
+    return (
+        <div>I am photo</div>
+    )
+}
