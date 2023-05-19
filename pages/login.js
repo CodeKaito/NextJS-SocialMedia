2429ed0c871ec9b8aa3964ebb572cf7735d8db93
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-05-19 11:37:54 +0200

    update

---
+++ pages/login.js
@@ -0,0 +1,7 @@
+import Layout from "@/components/Layout";
+
+export default function LoginPage() {
+    return (
+        <Layout hideNavigation={true}>login</Layout>
+    )
+}
--- pages/notifications.js
+++ pages/notifications.js
@@ -1,9 +1,43 @@
+import Avatar from "@/components/Avatar";
+import Card from "@/components/Card";
 import Layout from "@/components/Layout";
+import Link from "next/link";
 
 export default function Notifications() {
     return (
         <Layout>
-            Notifications
+            <h1 className='text-6xl mb-4 text-gray-400'>Notifications</h1>
+            <Card noPadding={true}>
+                <div className=''>
+                    <div className='flex gap-2 items-center border-b border-b-gray-100 p-4'>
+                        <Link href={'/profile'}>
+                            <Avatar />
+                        </Link>
+                        <div>
+                            <Link href={''} className={'font-semibold mr-1'}>John Doe</Link> liked your 
+                            <Link href={''} className={'text-socialBlue hover:underline ml-1'}>photo</Link>
+                        </div>
+                    </div>
+                    <div className='flex gap-2 items-center border-b border-b-gray-100 p-4'>
+                        <Link href={'/profile'}>
+                            <Avatar />
+                        </Link>
+                        <div>
+                            <Link href={''} className={'font-semibold mr-1'}>John Doe</Link> liked your 
+                            <Link href={''} className={'text-socialBlue hover:underline ml-1'}>photo</Link>
+                        </div>
+                    </div>
+                    <div className='flex gap-2 items-center border-b border-b-gray-100 p-4'>
+                        <Link href={'/profile'}>
+                            <Avatar />
+                        </Link>
+                        <div>
+                            <Link href={''} className={'font-semibold mr-1'}>John Doe</Link> liked your 
+                            <Link href={''} className={'text-socialBlue hover:underline ml-1'}>photo</Link>
+                        </div>
+                    </div>
+                </div>
+            </Card>
         </Layout>
     )
 }
